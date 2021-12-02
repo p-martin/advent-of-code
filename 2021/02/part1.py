2b0d47d9f8a200c38b8a4554cd9e859cd342b8be
p-martin <p_martin@gmx.net>
2021-12-02 06:21:46 +0100

    Add solutions for 2021, day 2

---
+++ 2021/02/part1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def process_content(content: str):
+    x = y = 0
+    for line in content.splitlines():
+        d, val = line.split()
+        val = int(val)
+        if d == 'forward':
+            x += val
+        y += {'down': 1, 'up': -1}.get(d, 0) * val
+    return x * y
+
+
+if __name__ == '__main__':
+    import os
+    input_file = os.path.join(os.path.dirname(__file__), 'input')
+    with open(input_file, encoding='utf-8') as f:
+        result = process_content(f.read())
+    print(f'result = {repr(result)}')
